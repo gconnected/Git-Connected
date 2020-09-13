8bc3949b3c8238381b0672795333d2924c30f060
morgan562 <tylermor562@gmail.com>
2020-09-12 22:38:20 -0700

    postman worked

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,26 +1,59 @@
 const fs = require('fs');
 const path = require('path');
-
+const db = require('../models/userModels.js');
 const fileController = {};
 
+fileController.createUser = (req, res, next) => {
+   // set variable that holds query string
+   // const users = ""
+}
 
+fileController.getEverything = (req, res, next) => {
+   const everything = 'SELECT * FROM users';
+   console.log('HELLO');
 
+   db.query(everything)
+   .then(data => {
+      console.log(data);
+      res.locals.everything = data.rows;
+      return next();
+   })
+   .catch(err => next(`{ log: 'Express error handler caught error in starWarsController.getSpecies',
+   status: 400,
+   message: { err: ${err} }`))
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fileController.searchUser = (req, res, next) => {
+   // console.log('req req req req req:', req);
+  let searched;
+   const { searchCompany, searchJob } = req.body;
+   // ==> searchCompany = 'Amazon'
+   console.log('searchCompany:', searchCompany);
+   const searchInput = [searchCompany, searchJob];
+   if (searchCompany && !searchJob) {
+     console.log(searchInput)
+      searched = 'SELECT users.* FROM users WHERE company_name = $1'
+   } else if (!searchCompany && searchJob) {
+      console.log(2)
+      searched = 'SELECT users.* FROM users WHERE company_name = $2'
+   } else if (searchCompany && searchJob) {
+     console.log(3)
+      searched = 'SELECT users.* FROM users WHERE company_name = $1 AND job = $2'
+   } else {
+      console.log('something')
+   }
+   console.log('searched:', searched, 'searchInput:', searchInput)
+   db.query(searched, [searchCompany])
+   .then((data) => {
+      console.log('return query', data.rows);
+      res.locals.userSearched = data.rows
+   })
+   .then(() => next())
+   .catch((err) =>
+      next(`{ log: 'Express error handler caught error in fileController.searchUser',
+      status: 400,
+      message: { err: ${err} } `)
+      ); 
+}
 
 module.exports = fileController;
--- server/server.js
+++ server/server.js
@@ -2,50 +2,36 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 const path = require('path');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const api = require('./routes/api');
 const cors = require('cors');
-const homeRouter = require('./routes/home')
-const loginRouter = require('./routes/login')
-const profileRouter = require('./routes/profile')
-const searchRouter = require('./routes/search')
-
 
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', api)
 
-
-app.use('/', express.static('../index.html'));
 // This will be used to serve our static file to the client 
+app.use('/', express.static('../index.html'));
 
 
+app.use((req, res) => res.sendStatus(404));
 
-
-
-app.use('/search', searchRouter );
-
-
-app.get('/profile', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './profile'))
-})
-
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './login'))
-})
-
-
-app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './home'))
-})
-
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: ' An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 });
-
-
-
-
 
 module.exports = app;
